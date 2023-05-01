783ad207af163dce55f5a6a8e39b093ec7852660
marina <marina@192.168.8.157>
2023-05-01 13:44:06 +0200

    código refactorizado y con la documentación generada con javadoc

--- src/Tarea_ED/CCuenta.java
+++ src/Tarea_ED/CCuenta.java
@@ -11,6 +11,41 @@ package Tarea_ED;
  */
 public class CCuenta {
 
+    /**
+     * @param cuenta the cuenta to set
+     */
+    public void setCuenta(String cuenta) {
+        this.cuenta = cuenta;
+    }
+
+    /**
+     * @return the saldo
+     */
+    public double getSaldo() {
+        return saldo;
+    }
+
+    /**
+     * @param saldo the saldo to set
+     */
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+    /**
+     * @return the tipoInterés
+     */
+    public double getTipoInterés() {
+        return tipoInterés;
+    }
+
+    /**
+     * @param tipoInterés the tipoInterés to set
+     */
+    public void setTipoInterés(double tipoInterés) {
+        this.tipoInterés = tipoInterés;
+    }
+
 
     // Propiedades de la Clase Cuenta
     private String nombre;
@@ -19,10 +54,23 @@ public class CCuenta {
     private double tipoInterés;
 
     /* Constructor sin argumentos */
+
+    /**
+     *
+     */
+
     public CCuenta ()
     {
     }
     // Constructor con parámetro para iniciar todas las propiedades de la clase
+
+    /**
+     *
+     * @param nom
+     * @param cue
+     * @param sal
+     * @param tipo
+     */
     public CCuenta (String nom, String cue, double sal, double tipo)
     {
         nombre=nom;
@@ -30,30 +78,52 @@ public class CCuenta {
         saldo=sal;
     }
    // Método para asignar el nombre del titular de la cuenta
+
+    /**
+     *
+     * @param nom
+     */
     public void setNombre(String nom)
     {
         nombre=nom;
     }
     // Método que me devuelve el nombre del titular
+
+    /**
+     *
+     * @return
+     */
     public String getNombre()
     {
         return nombre;
     }
 
     // Método que me devuelve el saldo disponible en cada momento
+
+    /**
+     *
+     * @return
+     */
      public double getEstado ()
     {
-        return saldo;
+        return getSaldo();
     }
 
     /* Método para ingresar cantidades en la cuenta. Modifica el saldo.
      * Este método va a ser probado con Junit
      */
+
+    /**
+     *
+     * @param cantidad
+     * @throws Exception
+     */
+
     public void ingresar(double cantidad) throws Exception
     {
         if (cantidad<0)
             throw new Exception("No se puede ingresar una cantidad negativa");
-        saldo = saldo + cantidad;
+        setSaldo(getSaldo() + cantidad);
     }
 
 
@@ -61,16 +131,28 @@ public class CCuenta {
     /* Método para retirar cantidades en la cuenta. Modifica el saldo.
      * Este método va a ser probado con Junit
      */
+
+    /**
+     *
+     * @param cantidad
+     * @throws Exception
+     */
+
     public void retirar (double cantidad) throws Exception
     {
         if (cantidad <= 0)
             throw new Exception ("No se puede retirar una cantidad negativa");
         if (getEstado()< cantidad)
             throw new Exception ("No se hay suficiente saldo");
-        saldo = saldo - cantidad;
+        setSaldo(getSaldo() - cantidad);
     }
 
     // Método que me devuelve el número de cuenta
+
+    /**
+     *
+     * @return
+     */
     public String getCuenta ()
     {
         return cuenta;
